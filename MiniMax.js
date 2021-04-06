520d973caaf520fc099428fe43d5bcf60e35dd6e
Mo. Mitwaly <62458010+MohammadMitwaly@users.noreply.github.com>
2021-04-06 20:11:01 +0300

    Add comments

--- MiniMax.js
+++ MiniMax.js
@@ -3,7 +3,7 @@ const bestPosition = () => {
   let bestMove = [];
   for (let i = 0; i < gameBoard[0].length; i++) {
     for (let j = 0; j < gameBoard[i].length; j++) {
-      // Is this position availabel to be played?
+      // Is this position available to be played?
       if (gameBoard[i][j] === "") {
         gameBoard[i][j] = AI_Player;
         let positionScore = minimax(gameBoard, 0, false);
@@ -16,15 +16,18 @@ const bestPosition = () => {
       }
     }
   }
+  // Updating the global gameBoard, could be improved by passing a refrence instead to the method
   gameBoard[bestMove[0]][bestMove[1]] = AI_Player;
   currentPlayer = Human_Player;
 };
 
+// Look-up map for the scores
 const Scores = {
   X: 1,
   O: -1,
   tie: 0,
 };
+
 
 const minimax = (gameBoard, depth, isMaximizing) => {
   let result = checkWinner();
@@ -34,6 +37,7 @@ const minimax = (gameBoard, depth, isMaximizing) => {
   }
 
   let bestScore;
+  // This is the maximizing player's turn 
   if (isMaximizing) {
     bestScore = -Infinity;
     for (let i = 0; i < gameBoard[0].length; i++) {
@@ -41,6 +45,7 @@ const minimax = (gameBoard, depth, isMaximizing) => {
         // Is this position availabel to be played?
         if (gameBoard[i][j] === "") {
           gameBoard[i][j] = AI_Player;
+          // Go deeper into the decision tree, not using alpha/beta pruning due to the small size of the tic-tac-toe decision tree
           let score = minimax(gameBoard, depth + 1, false);
           // Undo the position we just checked
           gameBoard[i][j] = "";
